6f61ada3ed307b4cf8916d5d215f82d658c580b1
Joseph Ramos <jardix22@gmail.com>
2017-11-29 20:32:43 -0500

    Added default index and about pages.

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+
+const Index = () => {
+  return (
+    <div>
+      <Header title="Bristou" subtitle="Theme" />
+      Hola!
+    </div>
+  )
+}
+
+export default Index
+
+// Node
+// const React = require('react')
+// module.exports Index
