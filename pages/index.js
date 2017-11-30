1e9d9caf586b997bb134d99d6025c992ff58512d
Yesica Ramirez <yesmagaly@gmail.com>
2017-11-29 21:05:58 -0500

    Added Jumbotron component to the index page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Header from '../components/Header'
+import Jumbotron from '../components/Jumbotron'
+
+const post = {
+  image: 'https://aloveroftheroad.files.wordpress.com/2016/07/unusual-weekend04.jpg',
+  tag: 'Adventures',
+  title: 'Unusual weekends',
+  comments: 12,
+  published: '29 Nov 2017'
+}
 
 const Index = () => {
   return (
     <div>
       <Header title="Bristou" subtitle="Theme" />
+      <Jumbotron {...post } />
       Hola!
     </div>
   )
