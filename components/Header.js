93ba3ca80af40d1ac22602fb5f64d96ae65d5bd9
Joseph Ramos <jardix22@gmail.com>
2017-12-02 20:47:26 -0500

    Added more styles to Header and Jumbotron components.

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,29 @@
 import styled from 'styled-components'
 
 const Content = styled.header`
+  padding: 2.5rem 0;
+`
+
+const H1 = styled.h1`
   text-transform: uppercase;
   text-align: center;
-  padding: 2rem 0;
+  font-size: 2.5rem;
+  font-weight: 500;
+  margin: 0;
+`
+
+const Link = styled.a`
+  color: black;
+  text-decoration: none;
+  border-bottom: 1px solid #c49b5f;
 `
 
 const Header = ({ title, subtitle }) => {
   return (
     <Content>
-      <h1>
-        { title }
-      </h1>
+      <H1>
+        <Link href="/">{ title }</Link>
+      </H1>
     </Content>
   )
 }
--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -2,7 +2,13 @@ import styled from 'styled-components'
 import Card from './Card'
 
 const Background = styled.div`
+  align-items: center;
   background-image: url(${props=>props.image});
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  height: 450px;
+  justify-content: center;
 `
 
 const Jumbotron = ({ image, ...rest }) => {
