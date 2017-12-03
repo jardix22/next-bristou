c0eaee0f96ad5c6164bd0db2dd2b167b35708515
Joseph Ramos <jardix22@gmail.com>
2017-12-02 20:46:16 -0500

    Updaded Card styles.

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background: rgba(243, 243, 241, 0.9);
+  padding: 1.5rem 1rem;
+  width: 30%;
+  box-sizing: border-box;
+  box-shadow: 0 0 0 8px rgba(255, 255, 255, .5);
+  text-align: center;
+`
+const Tag = styled.div`
+  font-style: italic;
+  color: #c49b5f;
+  font-size: 14px;
+  font-weight: 500;
+`
+
+const H2 = styled.h2`
+  text-transform: uppercase;
+`
+
+const Meta = styled.div`
+  display: flex;
+  justify-content: center;
+  color: #aeadac;
+  font-size: 13px;
+
+  & > span {
+    margin: 0 .5rem;
+  }
+`
+
 const Card = ({ tag, title, comments, published }) => {
   return (
-    <div>
-      <div>{ tag }</div>
-      <h2>{ title }</h2>
-      <div>
-        <span>{ comments }</span>
+    <Wrapper>
+      <Tag>{ tag }</Tag>
+      <H2>{ title }</H2>
+      <Meta>
+        <span>{ comments } Comments</span>
         <span>{ published }</span>
-      </div>
-    </div>
+      </Meta>
+    </Wrapper>
   )
 }
 
